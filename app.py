2018ddbcd63acad66e34fe4da6b5a44eb66db07a
natalia200124-star <natalia200124@gmail.com>
2026-02-15 14:04:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.set_page_config(
 # ==============================
 # AUTO-REFRESH
 # ==============================
-count = st_autorefresh(interval=8000, key="datarefresh")
+count = st_autorefresh(interval=20000, key="datarefresh")
 
 # ==============================
 # CARGA DE DATOS DESDE API (APPS SCRIPT)
